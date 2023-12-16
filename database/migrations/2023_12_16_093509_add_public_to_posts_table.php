6326f89ca444ffe9b665867daa2e3921e710258d
Crxa <carakawidi07@gmail.com>
2023-12-16 16:36:07 +0700

    add feature public and private article

---
+++ database/migrations/2023_12_16_093509_add_public_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->boolean('public')->default(true);
+        });
+
+        // Menetapkan nilai default untuk catatan yang sudah ada
+        DB::table('posts')->update(['public' => true]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
