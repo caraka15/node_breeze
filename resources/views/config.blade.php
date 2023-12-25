860f793e537bbce9cc9918c0da542d4b6a25e557
Crxa <carakawidi07@gmail.com>
2023-12-25 10:08:24 +0700

    fix copy button in config

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -4,7 +4,7 @@
     @if (session()->has('success'))
         <!-- Alert Container -->
         <div id="alert"
-            class="relative flex justify-between top-0 right-0 m-4 p-4 bg-green-500 text-white rounded shadow-lg">
+            class="relative right-0 top-0 m-4 flex justify-between rounded bg-green-500 p-4 text-white shadow-lg">
             <p>{{ session('success') }}</p>
             <button type="button" id="closeBtn" class="close transition" data-dismiss="alert">
                 <i class="text-white" data-feather="x"></i>
@@ -13,9 +13,9 @@
     @endif
 
     <div class="py-6">
-        <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="bg-white dark:bg-gray-800 shadow sm:rounded-lg p-4 sm:p-8">
-                <div class="dark:text-white overflow-x-auto">
+        <div class="mx-auto space-y-6 sm:px-6 lg:px-8">
+            <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
+                <div class="overflow-x-auto dark:text-white">
                     <table class="min-w-full border-collapse border border-slate-500 text-center">
                         <thead>
                             <tr>
@@ -28,12 +28,12 @@
                         <tbody>
                             @foreach ($configs as $config)
                                 <tr>
-                                    <td class="border border-slate-700 relative py-2">
+                                    <td class="relative border border-slate-700 py-2">
                                         <div class="text-sm">
                                             {{ $config->name }}
                                             @if (now()->diffInDays($config->updated_at) < 5)
                                                 <span
-                                                    class="absolute left-0 top-0 text-xs px-0.5 py-0.5 text-white bg-red-500 border rounded-full">New</span>
+                                                    class="absolute left-0 top-0 rounded-full border bg-red-500 px-0.5 py-0.5 text-xs text-white">New</span>
                                             @endif
                                         </div>
                                     </td>
@@ -51,13 +51,13 @@
                                         <div class="flex justify-center">
                                             @if ($config->type === 'vmess')
                                                 <button
-                                                    class="bg-blue-600 w-12 h-8 px-3 py-1 me-1 rounded-md hover:bg-blue-500"
+                                                    class="me-1 h-8 w-12 rounded-md bg-blue-600 px-3 py-1 hover:bg-blue-500"
                                                     onclick="copyToClipboard('{{ $config->config }}')">
                                                     <i data-feather="copy"></i>
                                                 </button>
                                             @else
                                                 <a href="{{ asset('storage/uploads/' . $config->config) }}"
-                                                    class="bg-orange-600 w-12 h-8 px-3 py-1 me-1 rounded-md hover:bg-orange-500"><i
+                                                    class="me-1 h-8 w-12 rounded-md bg-orange-600 px-3 py-1 hover:bg-orange-500"><i
                                                         data-feather="download"></i></a>
                                             @endif
                                         </div>
@@ -70,4 +70,37 @@
             </div>
         </div>
     </div>
+    <script>
+        function copyToClipboard(text) {
+            var textarea = document.createElement("textarea");
+            textarea.value = text;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textarea);
+
+            // Show popup
+            showAlert('Config copied to clipboard!');
+        }
+
+        function showAlert(message) {
+            // Create alert div with Tailwind CSS classes
+            var alertDiv = document.createElement("div");
+            alertDiv.className = "fixed top-16 right-0 p-4 bg-green-500 text-white";
+            alertDiv.textContent = message;
+
+            // Append alert div to body
+            document.body.appendChild(alertDiv);
+
+            // Hide the alert after 3 seconds
+            setTimeout(function() {
+                // Use opacity to animate the hide effect
+                alertDiv.style.opacity = 0;
+                // Remove the alert div from the DOM after the animation
+                setTimeout(function() {
+                    document.body.removeChild(alertDiv);
+                }, 500);
+            }, 3000);
+        }
+    </script>
 </x-app-layout>
