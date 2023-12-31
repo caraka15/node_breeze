9826a8fe0c027a949e9308e6e7c88bf92fe8156d
Crxa <carakawidi07@gmail.com>
2023-12-31 14:49:26 +0700

    make stats exorde

--- public/php/exorde-stats.php
+++ public/php/exorde-stats.php
@@ -2,12 +2,42 @@
 
 $userAddress = $_GET['user_address'];
 
+// Set absolute paths for files in public/php/
+$previousLeaderboardPath = __DIR__ . '/previous_leaderboard.json';
+$lastLeaderboardUpdatePath = __DIR__ . '/last_leaderboard_update_timestamp.txt';
+
+// Load previous leaderboard data and timestamp
+$previousLeaderboardData = json_decode(file_get_contents($previousLeaderboardPath), true);
+$lastLeaderboardUpdateTimestamp = file_get_contents($lastLeaderboardUpdatePath);
+
+// Get the current timestamp
+$currentTimestamp = time();
+
+// Calculate the time difference in seconds
+$timeDifference = $currentTimestamp - $lastLeaderboardUpdateTimestamp;
+
+// Convert seconds to minutes
+$timeDifferenceInMinutes = round($timeDifference / 60);
+
+// Fetch previous leaderboard if more than 10 minutes have passed
+if ($timeDifferenceInMinutes > 10) {
+    // Fetch the previous leaderboard
+    // ... (your code to fetch the leaderboard)
+
+    // Save the current timestamp for the next comparison
+    file_put_contents($lastLeaderboardUpdatePath, $currentTimestamp);
+
+    // Save the current leaderboard data for the next 10-minute interval
+    file_put_contents($previousLeaderboardPath, json_encode($leaderboardData));
+}
+
 // Fetch Data
 $dailyUrl = "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/Stats/daily_EXD_projected_rewards.json";
 $hourlyUrl = "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/Stats/hourly_EXD_projected_rewards.json";
 $leaderboardUrl = "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/Stats/leaderboard.json";
 $bountyUrl = "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/Stats/bounties.json";
 
+// Fetch current data
 $dailyData = json_decode(file_get_contents($dailyUrl), true);
 $hourlyData = json_decode(file_get_contents($hourlyUrl), true);
 $leaderboardData = json_decode(file_get_contents($leaderboardUrl), true);
@@ -16,21 +46,79 @@ $bountyData = json_decode(file_get_contents($bountyUrl), true);
 $userRep = $leaderboardData[$userAddress] ?? 0;
 $userBounty = $bountyData['tweets'][$userAddress] ?? 0;
 
-$sortedLeaderboard = array_values(array_flip(array_flip(array_column($leaderboardData, null))));
+// Mengurutkan dari yang tertinggi ke terendah berdasarkan reputasi
+arsort($leaderboardData);
+
+// Mengambil kunci (alamat) dari array yang sudah diurutkan
+$sortedLeaderboard = array_keys($leaderboardData);
+
+// Menentukan urutan untuk setiap alamat
 $rankedLeaderboard = array_flip($sortedLeaderboard);
 
+// Menggeser urutan untuk memastikan dimulai dari 1
 $rankedLeaderboard = array_map(function ($rank) {
     return $rank + 1;
 }, $rankedLeaderboard);
 
+// Mendapatkan peringkat pengguna yang diminta
 $userRank = $rankedLeaderboard[$userAddress] ?? null;
 
 $totalRep = array_sum($leaderboardData);
 $totalBounty = array_sum($bountyData['tweets']);
-$exdPrice = 0.0647;
+
+
+// Save the current leaderboard data for the next comparison
+file_put_contents($previousLeaderboardPath, json_encode($leaderboardData));
+
+// ... (rest of your code)
+
+// Get cryptocurrency data
+function getCryptoData()
+{
+    $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
+    $api_key = 'c278f052-8555-4544-81a5-a2d0ef287c4c';
+    $crypto_slug = 'exorde';
+    $convert_currency = 'USD';
+
+    $parameters = [
+        'slug' => $crypto_slug,
+        'convert' => $convert_currency,
+    ];
+
+    $headers = [
+        'Accepts: application/json',
+        'X-CMC_PRO_API_KEY: ' . $api_key,
+    ];
+
+    $options = [
+        'http' => [
+            'header' => implode("\r\n", $headers),
+        ],
+    ];
+
+    $context = stream_context_create($options);
+    $response = file_get_contents("$url?" . http_build_query($parameters), false, $context);
+
+    $crypto_data = json_decode($response, true);
+
+    return $crypto_data;
+}
+
+// Get cryptocurrency data
+$cryptoData = getCryptoData();
+
+// Extract relevant information
+$bitcoinInfo = $cryptoData['data']['23638'];
+
+$exdPrice = number_format($bitcoinInfo['quote']['USD']['price'], 4);
+
 $hourlyReward = $hourlyData[$userAddress] ?? 0;
 $monthlyReward = ($dailyData[$userAddress] ?? 0) * 30;
-$hourlyRepIncrease = 506;
+$hourlyRepIncrease = $leaderboardData[$userAddress] - ($previousLeaderboardData[$userAddress]);
+$usdReward = $monthlyReward * $exdPrice;
+
+// Create a string for "minutes ago" information
+$minutesAgoString = $timeDifferenceInMinutes > 0 ? $timeDifferenceInMinutes . ' minutes ago' : 'just now';
 
 // Membuat array data untuk dikirim sebagai JSON
 $data = [
@@ -43,7 +131,9 @@ $data = [
     'exdPrice' => $exdPrice,
     'hourlyReward' => $hourlyReward,
     'monthlyReward' => $monthlyReward,
+    'usdReward' => $usdReward,
     'hourlyRepIncrease' => $hourlyRepIncrease,
+    'lastUpdate' => $minutesAgoString, // Add last update time
 ];
 
 // Mengembalikan data sebagai JSON
--- resources/views/exorde/index.blade.php
+++ resources/views/exorde/index.blade.php
@@ -2,61 +2,93 @@
     @section('title', 'Exorde Stats')
     @section('description', 'Exorde Stats Node Service, Validator guide')
 
-    <div class="mx-auto mt-5 max-w-lg rounded bg-white p-8 shadow">
-        <label for="user_address">Enter Address:</label>
-        <input type="text" id="user_address" required>
-        <button onclick="getUserStats()">Get Stats</button>
+    <div class="py-12">
+        <div class="mx-auto max-w-5xl space-y-6 sm:px-6 lg:px-8">
+            <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
 
-        <div id="statsContainer" class="hidden rounded-md bg-gray-200 p-4">
-            <p id="userAddress"></p>
-            <p id="userRep"></p>
-            <p id="userRank"></p>
-            <p id="userBounty"></p>
-            <p id="totalRep"></p>
-            <p id="totalBounty"></p>
-            <p id="exdPrice"></p>
-            <p id="hourlyReward"></p>
-            <p id="monthlyReward"></p>
-            <p id="hourlyRepIncrease"></p>
+                <div class="flex items-center justify-center">
+                    <input type="text"
+                        class="max-w-full rounded-l-md border-gray-300 shadow-none focus:border-orange-500 focus:ring-orange-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-orange-600 dark:focus:ring-orange-600 sm:w-96"
+                        placeholder="Get stats" name="user_address" id="user_address">
+                    <button
+                        class="inline-flex items-center rounded-r-md border border-transparent bg-gray-800 p-3 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 dark:bg-orange-500 dark:hover:bg-orange-600 dark:focus:bg-orange-600 dark:active:bg-orange-600"
+                        type="submit" onclick="getUserStats()">Get Stats</button>
+                </div>
+
+
+
+                <div id="statsContainer"
+                    class="mt-4 hidden rounded-md bg-gray-200 p-4 dark:bg-slate-500 dark:text-white">
+                    <p id="userAddress"></p>
+                    <p id="userRep"></p>
+                    <p id="userRank"></p>
+                    <p id="userBounty"></p>
+                    <br>
+                    <p id="totalRep"></p>
+                    <p id="totalBounty"></p>
+                    <br>
+                    <p id="exdPrice"></p>
+                    <p id="hourlyReward"></p>
+                    <p id="monthlyReward"></p>
+                    <p id="usdReward"></p>
+
+                    <!-- Menambahkan elemen untuk menampilkan waktu terakhir diperbarui -->
+                </div>
+
+                <div id="loadingMessage" class="hidden w-full">
+                    <div class="mx-auto mt-5 flex h-20 w-20 content-center items-center justify-center">
+                        <svg version="1.1" id="L5" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"
+                            enable-background="new 0 0 0 0" xml:space="preserve">
+                            <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+                                <animateTransform attributeName="transform" dur="1s" type="translate"
+                                    values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1" />
+                            </circle>
+                            <circle fill="#fff" stroke="none" cx="30" cy="50" r="6">
+                                <animateTransform attributeName="transform" dur="1s" type="translate"
+                                    values="0 10 ; 0 -10; 0 10" repeatCount="indefinite" begin="0.2" />
+                            </circle>
+                            <circle fill="#fff" stroke="none" cx="54" cy="50" r="6">
+                                <animateTransform attributeName="transform" dur="1s" type="translate"
+                                    values="0 5 ; 0 -5; 0 5" repeatCount="indefinite" begin="0.3" />
+                            </circle>
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div id="loadingMessage" class="hidden">
-            Loading...
-        </div>
-    </div>
 
-    <script>
-        async function getUserStats() {
-            var userAddress = document.getElementById('user_address').value;
-            var statsContainer = document.getElementById('statsContainer');
-            var loadingMessage = document.getElementById('loadingMessage');
 
-            loadingMessage.style.display = 'block';
+        <script>
+            async function getUserStats() {
+                var userAddress = document.getElementById('user_address').value;
+                var statsContainer = document.getElementById('statsContainer');
+                var loadingMessage = document.getElementById('loadingMessage');
 
-            try {
-                // Pengambilan data langsung dari server
-                var response = await fetch(`/php/exorde-stats.php?user_address=${userAddress}`);
+                loadingMessage.style.display = 'block';
 
-                var data = await response.json();
+                try {
+                    var response = await fetch(`/php/exorde-stats.php?user_address=${userAddress}`);
+                    var data = await response.json();
 
-                document.getElementById('userAddress').innerText = 'Address: ' + data.userAddress;
-                document.getElementById('userRep').innerText = 'Reputation: ' + data.userRep;
-                document.getElementById('userRank').innerText = 'Rank: ' + data.userRank;
-                document.getElementById('userBounty').innerText = 'Bounty: ' + data.userBounty;
-                document.getElementById('totalRep').innerText = 'Total Reputation: ' + data.totalRep;
-                document.getElementById('totalBounty').innerText = 'Total Bounty: ' + data.totalBounty;
-                document.getElementById('exdPrice').innerText = 'EXD Price: ' + data.exdPrice;
-                document.getElementById('hourlyReward').innerText = 'Hourly Reward: ' + data.hourlyReward;
-                document.getElementById('monthlyReward').innerText = 'Monthly Reward: ' + data.monthlyReward;
-                document.getElementById('hourlyRepIncrease').innerText = 'Hourly Rep Increase: ' + data
-                    .hourlyRepIncrease;
+                    document.getElementById('userAddress').innerText = 'Address: ' + data.userAddress;
+                    document.getElementById('userRep').innerText = 'Reputation: ' + data.userRep;
+                    document.getElementById('userRank').innerText = 'Rank: ' + data.userRank;
+                    document.getElementById('userBounty').innerText = 'Bounty: ' + data.userBounty + ' REP';
+                    document.getElementById('totalRep').innerText = 'Total Reputation: ' + data.totalRep + ' REP';
+                    document.getElementById('totalBounty').innerText = 'Total Bounty: ' + data.totalBounty + ' REP';
+                    document.getElementById('exdPrice').innerText = 'EXD Price: ' + data.exdPrice + 'USD';
+                    document.getElementById('hourlyReward').innerText = 'Hourly Reward: ' + data.hourlyReward + ' EXD';
+                    document.getElementById('monthlyReward').innerText = 'Monthly Reward: ' + data.monthlyReward + ' EXD';
+                    document.getElementById('usdReward').innerText = 'USD Monthly Reward: ' + data.usdReward + ' USD';
 
-                loadingMessage.style.display = 'none';
-                statsContainer.style.display = 'block';
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                loadingMessage.innerText = 'Error fetching data';
+                    loadingMessage.style.display = 'none';
+                    statsContainer.style.display = 'block';
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    loadingMessage.innerText = 'Error fetching data';
+                }
             }
-        }
-    </script>
+        </script>
 </x-app-layout>
