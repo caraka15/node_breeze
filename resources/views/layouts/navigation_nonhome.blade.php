9f2bef546f3cbdf40a234b45daae90a289835f22
Crxa <carakawidi07@gmail.com>
2024-05-20 20:33:21 +0700

    update

--- resources/views/layouts/navigation_nonhome.blade.php
+++ resources/views/layouts/navigation_nonhome.blade.php
@@ -147,7 +147,7 @@
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('user')" :active="request()->routeIs('user')">
+                <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                     {{ __('Users') }}
                 </x-responsive-nav-link>
                 @can('admin')
