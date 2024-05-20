85b0ea59644ed5f522bc72df5691c60e56030eab
Crxa <carakawidi07@gmail.com>
2024-05-20 23:32:09 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
         $total = $user->count();
         return view('dashboard.users.index', [
             'total' => $total,
-            'users' => User::all()->latest('created_at')->get(),
+            'users' => User::all(),
             'title' => "Users Dashboard"
         ]);
     }
