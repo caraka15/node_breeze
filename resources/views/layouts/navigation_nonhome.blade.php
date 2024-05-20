6aa16e23dd64f187a164c8c138923a9020be5d39
Crxa <carakawidi07@gmail.com>
2024-05-20 23:19:54 +0700

    update

--- resources/views/layouts/navigation_nonhome.blade.php
+++ resources/views/layouts/navigation_nonhome.blade.php
@@ -37,7 +37,7 @@
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
-                        <x-nav-link :href="route('user')" :active="request()->routeIs('user')">
+                        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                             {{ __('Users') }}
                         </x-nav-link>
                         @can('admin')
